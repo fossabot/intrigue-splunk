2758437f6597ecd9ed1228dac62fbd5652eee869
Duarte <duartemateus@outlook.com>
2020-10-19 14:43:14 +0100

    Initial commit.
    Adds project files
    Adds compressed version (for development)
    Adds compiled version (for use)
    
    This two should be moved to a more appropiate place instead of the root folder.
    
    This is not the final strucutre.

---
+++ Intrigue/bin/Intrigue_rh_Intrigue.py
@@ -0,0 +1,103 @@
+
+import intrigue_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    DataInputModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'interval',
+        required=True,
+        encrypted=False,
+        default=False,
+        validator=validator.Pattern(
+            regex=r"""^(21[6-8][0-9]{2}|219[0-8][0-9]|2199[0-9]|2[2-9][0-9]{3}|3[0-9]{4}|4[0-2][0-9]{3}|43[01][0-9]{2}|43200)$""",
+        )
+    ),
+    field.RestField(
+        'index',
+        required=True,
+        encrypted=False,
+        default='default',
+        validator=validator.String(
+            min_len=1,
+            max_len=80,
+        )
+    ),
+    field.RestField(
+        'collection_name',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            min_len=0,
+            max_len=8192,
+        )
+    ),
+    field.RestField(
+        'item_type',
+        required=True,
+        encrypted=False,
+        default='entities',
+        validator=None
+    ),
+    field.RestField(
+        'start_date',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.Pattern(
+            regex=r"""^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$""",
+        )
+    ),
+    field.RestField(
+        'access_key',
+        required=True,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            min_len=0,
+            max_len=8192,
+        )
+    ),
+    field.RestField(
+        'secret_key',
+        required=True,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            min_len=0,
+            max_len=8192,
+        )
+    ),
+
+    field.RestField(
+        'disabled',
+        required=False,
+        validator=None
+    )
+
+]
+model = RestModel(fields, name=None)
+
+
+endpoint = DataInputModel(
+    'Intrigue',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
---
+++ Intrigue/bin/input_module_Intrigue.py
@@ -0,0 +1,115 @@
+
+# encoding = utf-8
+
+import os
+import sys
+import time
+import json
+import datetime
+
+
+class Header:
+
+    def __init__(self, access_key, secret_key):
+
+        self.Authorization = {
+            "INTRIGUE_ACCESS_KEY": access_key,
+            "INTRIGUE_SECRET_KEY": secret_key
+        }
+
+
+class UrlBuilder:
+    def build(self):
+        return "https://app.intrigue.io/api/collections/{}/export/{}/from_date/{}/".format(self.collection, self.issueType, self.startDate)
+
+    def __init__(self, collection, issueType, startDate):
+
+        self.collection = collection
+        self.issueType = issueType
+        self.startDate = startDate
+
+
+def buildJsonResult(helper, jsonResult):
+    items = []
+
+    for item in jsonResult['result']['items']:
+        state = helper.get_check_point(item["id"])
+        if state is None:
+            items.append(item)
+            helper.save_check_point(item["id"], "Indexed")
+        # else: #this would be used to delete an existing entry and add a new one. Possibly to update a value.
+        #     helper.delete_check_point(item["id"])
+        #     items.append(item)
+        #     helper.save_check_point(item["id"], "Indexed")
+        # UNCOMMENT THIS LINE WHILE TESTING
+        helper.delete_check_point(item["id"])
+    return items
+
+
+'''
+    IMPORTANT
+    Edit only the validate_input and collect_events functions.
+    Do not edit any other part in this file.
+    This file is generated only once when creating the modular input.
+'''
+'''
+# For advanced users, if you want to create single instance mod input, uncomment this method.
+def use_single_instance_mode():
+    return True
+'''
+
+
+def validate_input(helper, definition):
+    # This example accesses the modular input variable
+    # collection_name = definition.parameters.get('collection_name', None)
+    # item_type = definition.parameters.get('item_type', None)
+    # date = definition.parameters.get('date', None)
+    pass
+
+
+def collect_events(helper, ew):
+    loglevel = helper.get_log_level()
+    # (log_level can be "debug", "info", "warning", "error" or "critical", case insensitive)
+    helper.set_log_level(loglevel)
+    opt_collection_name = helper.get_arg('collection_name')
+    opt_item_type = helper.get_arg('item_type')
+    opt_date = helper.get_arg('start_date')
+    opt_access_key = helper.get_arg("access_key")
+    opt_secret_key = helper.get_arg("secret_key")
+    helper.log_debug("collected data from fields")
+    # get input type
+    try:
+        header = Header(opt_access_key, opt_secret_key)
+        url = UrlBuilder(opt_collection_name, opt_item_type, opt_date)
+    except:
+        helper.log_debug("Error occurred when creating the request.")
+        raise Exception("Error occurred when creating the request.")
+
+    helper.log_debug("built url and headers")
+    response = helper.send_http_request(url.build(),
+                                        'GET',
+                                        parameters=None,
+                                        payload=None,
+                                        headers=header.Authorization,
+                                        cookies=None,
+                                        verify=True,
+                                        cert=None,
+                                        timeout=None,
+                                        use_proxy=True)
+    helper.log_debug("got response back from API")
+    r_status = response.status_code
+    helper.log_debug(f"got status code back from API {r_status}")
+    if r_status != 200:
+        response.raise_for_status()
+        raise Exception("Error occurred when retrieving data.")
+    # get response body as json. If the body text is not a json string, raise a ValueError
+    r_json = response.json()
+    helper.log_debug("started building json")
+    items = buildJsonResult(helper, r_json)
+    helper.log_debug("finished building json")
+    # To create a splunk event
+    event = helper.new_event(json.dumps(items), time=None, host=None,
+                             index=None, source=None, sourcetype=None, done=True, unbroken=True)
+    helper.log_debug("created new event")
+    ew.write_event(event)
+    helper.log_debug("finished writing event")
